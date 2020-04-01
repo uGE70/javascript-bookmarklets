c4e8d995c5edda339e33ae91af07decdeaf48151
Christophe Lance <info@internet-marketer.ch>
2020-04-01 11:36:35 +0200

    Fix text

--- check-keywords-links.js
+++ check-keywords-links.js
@@ -1,1 +1,1 @@
-javascript:let input = window.prompt('Check for this keyword in any links:');let a = document.getElementsByTagName('a');for(let i=0; i<a.length; i++){let s = a[i].href;console.log(s);if (s && s.indexOf(input) != -1) {a[i].style.backgroundColor = 'red';}};
+javascript:let input = window.prompt('Check for this keyword in any page links:');let a = document.getElementsByTagName('a');for(let i=0; i<a.length; i++){let s = a[i].href;console.log(s);if (s && s.indexOf(input) != -1) {a[i].style.backgroundColor = 'red';}};
